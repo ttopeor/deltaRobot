921e4d967a4b21288b4152df3084164f33e4c07b
ttopeor <ttopeor@udel.edu>
2021-12-11 12:09:40 +0800

    regular update

---
+++ scripts/get_encoder_position.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+NAME = 'get_encoder_position_server'
+
+from deltarobot.srv import *
+import rospy
+import os
+
+import serial
+import sys
+#import binascii
+import time
+ser = serial.Serial('/dev/ttyUSB0',timeout=0.04)  # open serial port
+print(ser.name)         # check which port was really used
+
+def write_req(ser,encoder_num):
+    req1 = b'\x9A\x03\xB6\xC1\x00\x02\xAF\x94'
+    req2 = b'\x01\x03\xA3\x48\x00\x02\x66\x59'
+    req3 = b'\x76\x03\xA4\x48\x00\x02\x6D\xAA'
+    req = [req1,req2,req3]
+    ser.write(req[encoder_num-1])
+    res = ser.read(9)
+
+    while (res == b''):
+        ser.write(req[encoder_num-1])
+        res = ser.read(9)
+        time.sleep(0.01)
+        #print("RuntimeError ID:",encoder_num)
+
+    res = res[5:7]
+    #print(binascii.hexlify(bytearray(res)))
+    res = int.from_bytes(res, "big")
+    return res
+    # print(res)
+
+def get_encoder_position(req):
+    #encoder_pos1 = round(write_req(ser,1)*300/1023,2)
+    #encoder_pos2 = round(write_req(ser,2)*300/1023,2)
+    #encoder_pos3 = round(write_req(ser,3)*300/1023,2)
+    encoder_pos1 = write_req(ser,1)
+    encoder_pos2 = write_req(ser,2)
+    encoder_pos3 = write_req(ser,3)
+    return GetEncoderPositionResponse(encoder_pos1,encoder_pos2,encoder_pos3)
+
+
+def get_encoder_position_server():
+    rospy.init_node(NAME)
+    s = rospy.Service('get_encoder_position', GetEncoderPosition, get_encoder_position)
+    print ("Ready to get positons of Encoders")
+    # spin() keeps Python from exiting until node is shutdown
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        get_encoder_position_server()
+    except rospy.ROSInterruptException:
+        ser.close()             # close port
+        print('port closed')
+        pass
